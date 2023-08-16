92924db62290f114311d825b7585fb86e1463a80
Ryan Sword <rysword1@gmail.com>
2023-08-16 18:40:05 -0400

    updated seed file

--- backend/config.js
+++ backend/config.js
@@ -15,7 +15,6 @@ function getDatabaseUri() {
 
 // const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
-console.log(process.env);
 console.log("Deckbuilder Config:".green);
 console.log("SECRET_KEY:".yellow, SECRET_KEY);
 console.log("PORT:".yellow, PORT.toString());

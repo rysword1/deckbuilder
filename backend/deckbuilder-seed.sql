a6a790bc2bafe8881c4c122020031142d6d2ebae
Ryan Sword <rysword1@gmail.com>
2023-08-28 18:19:54 -0400

    updated seed file

--- backend/deckbuilder-seed.sql
+++ backend/deckbuilder-seed.sql
@@ -1,5 +1,5 @@
 INSERT INTO decks (title, date_created, card_ids)
 
-VALUES ('deck1', '2023-08-01', '["004adf9a-b59a-5d56-9093-df73b9929bb1"]'),
-       ('deck2', '1999-12-12', '["345d54cc-021c-558d-abb1-026ef6d67ced", "5f8287b1-5bb6-5f4c-ad17-316a40d5bb0c"]'),
-       ('deck3', '2023-08-23', '["5f8287b1-5bb6-5f4c-ad17-316a40d5bb0c"]');
+VALUES ('deck1', '2023-08-01', '["5909e77e-a930-4713-bca4-c6b265238c17"]'),
+       ('deck2', '1999-12-12', '["5c83074d-0c9b-4b58-94ca-d75240485579", "f80cfcad-c82b-461d-8002-a8e1acddacf2"]'),
+       ('deck3', '2023-08-23', '["f8c2e662-d8c3-44d4-b666-fe0d4a046271"]');
